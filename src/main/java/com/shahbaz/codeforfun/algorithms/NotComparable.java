ac23c0bcc96889faeef494739c6316ffe07c89d1
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2015-02-01 00:15:13 +0530

    NotComparable Class

---
+++ src/main/java/com/shahbaz/codeforfun/algorithms/NotComparable.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.shahbaz.codeforfun.algorithms;
+
+/**
+ * This class will not implement Comparable interface hence while sorting it
+ * will throw a <code>ClassCastException</code>
+ * 
+ * @author Shahbaz.Khan
+ *
+ *
+ */
+public class NotComparable {
+
+	private int i;
+
+	public NotComparable(final int i) {
+		this.i = i;
+	}
+}
