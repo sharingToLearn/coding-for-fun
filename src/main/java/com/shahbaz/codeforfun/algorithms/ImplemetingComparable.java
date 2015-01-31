1eb51adae98b8c9e21bac7f38c4b1673fb44cd5e
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2015-02-01 01:52:00 +0530

    Added Testcase for Comparable Class

---
+++ src/main/java/com/shahbaz/codeforfun/algorithms/ImplemetingComparable.java
@@ -0,0 +1,47 @@
+package com.shahbaz.codeforfun.algorithms;
+
+/**
+ * Class implementing the <code>Comparable</code> interface so it has access to
+ * the compareTo method wherein the logic of comparing can be written.
+ * 
+ * @author Shahbaz.Khan
+ *
+ */
+public class ImplemetingComparable implements Comparable<ImplemetingComparable> {
+
+	private int i;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public ImplemetingComparable(final int i) {
+		this.i = i;
+	}
+
+	/**
+	 * returns positive value if current objetc's 1 is greater than paramObj
+	 * negative if lesser and zero if both are equal
+	 */
+	public int compareTo(ImplemetingComparable paramObj) {
+
+		int compareValeOfi = ((ImplemetingComparable) paramObj).getI();
+		return this.i - compareValeOfi;
+	}
+	
+	/*================= Disadvantages ========================
+	 * 1. Will only sort based on one logic as there is only one
+	 * compareTo() method.
+	 * 
+	 * 2. If Source code of the class is not available or if the
+	 * class is final nothing can be done to make it or its
+	 * sub classes implement comparable
+	 * 
+	 * 
+	 *========================================================*/
+
+}
--- src/test/java/com/shahbaz/codeforfun/algorithms/Sorting.java
+++ src/test/java/com/shahbaz/codeforfun/algorithms/Sorting.java
@@ -1,12 +1,17 @@
 package com.shahbaz.codeforfun.algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * The story of Comparable and Comparator
+ * The story of <code>Comparable</code> and <code>Comparator<code>
  * 
  * @author Shahbaz.Khan
  *
@@ -39,6 +44,49 @@ public class Sorting {
 
 		Arrays.sort(orginal);
 		Assert.assertArrayEquals(expected, orginal);
+
+	}
+
+	/*
+	 * Sorting a class which does not implement Comparable will always throw a
+	 * ClassCastException
+	 */
+	@Test(expected = ClassCastException.class)
+	public void sortingAnObjectWhichDoesNotImplementComparable() {
+		List<NotComparable> lNoComparables = new ArrayList<NotComparable>();
+		for (int i = 0; i < 10; i++) {
+			lNoComparables.add(new NotComparable(i));
+		}
+
+		Arrays.sort(lNoComparables.toArray());
+
+		// Collects.sort() will directly throw a compilation failure since it
+		// does not implements Comparable
+		// Collections.sort(lNoComparables);
+
+	}
+
+	/*
+	 * Sorting works perfectly after implementing Comparable
+	 * ClassCastException
+	 */
+	@Test
+	public void sortingAnObjectWhichDoesImplementComparable() {
+
+		List<ImplemetingComparable> lExpected = new ArrayList<ImplemetingComparable>();
+		lExpected.add(new ImplemetingComparable(2));
+		lExpected.add(new ImplemetingComparable(3));
+		lExpected.add(new ImplemetingComparable(4));
+
+		List<ImplemetingComparable> lComparables = new ArrayList<ImplemetingComparable>();
+		lComparables.add(new ImplemetingComparable(4));
+		lComparables.add(new ImplemetingComparable(3));
+		lComparables.add(new ImplemetingComparable(2));
+		Collections.sort(lComparables);
+
+		/*
+		 * Need to find a way to assert on List of objects
+		 */
 
 	}
 
